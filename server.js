375d560d8d248b8c19ba05f6fd96a4ad6db5b063
aabazary <aabazary@gmail.com>
2021-08-22 00:35:41 -0700

    trouble showing the notes

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const db = require('./db/db.json')
-const { v4: uuidv4 } = require('uuid');
+const {
+    v4: uuidv4
+} = require('uuid');
 
 const PORT = process.env.port || 3001;
 
@@ -10,54 +12,55 @@ const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.static('public'));
 
 
-app.get('/notes',(req,res) =>{
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-app.get('*',(req,res) =>{
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 // GET Route for notes page
-app.get('/api/notes', (req, res) =>{
-    res.json(db);}
-  );
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+});
 
-function createNote(body, event){
-    let noteEl = {
-        id: uuidv4(), 
+
+app.post('/api/notes', (req, res) => {
+
+    const noteEl = createNote(req.body, db);
+    res.json(noteEl);
+})
+
+function createNote(body, event) {
+    const noteEl = {
+        id: uuidv4(),
         title: body.title,
         text: body.text
 
     };
     let noteArr = event || [];
-    console.log('noteEl',noteEl)
-    console.log('noteArr',noteArr)
-    console.log('event',event)
-    console.log('body',body)
     noteArr.push(noteEl);
- 
+
 
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
         JSON.stringify(noteArr)
     );
+    console.log(noteEl)
     return noteEl;
-}
-app.post('/api/notes', (req,res) =>{
-
-    const noteEl = createNote(req.body, db); 
-    res.json(noteEl);
-})
+};
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
